e42d0f4e47bc2311812148573abdfa2074e480ea
b0nk <fsantos.pt@hotmail.com>
2025-12-15 01:44:58 +0000

    2025 - day05 part2

--- 2025/day05.c
+++ 2025/day05.c
@@ -8,6 +8,7 @@ struct Node {
 	unsigned long min;
 	unsigned long max;
 	Node* next;
+	Node* prev;
 };
 
 Node* create_node(unsigned long x, unsigned long y) {
@@ -17,10 +18,14 @@ Node* create_node(unsigned long x, unsigned long y) {
 	return n;
 }
 
+int in_range(unsigned long value, Node* range){
+	return (value >= range->min) && (value <= range->max);
+}
+
 int is_fresh(unsigned long id, Node* head){
 	Node* curr = head;
 	while(curr){
-		if(id >= curr->min && id <= curr->max){
+		if(in_range(id, curr)){
 			return 1;
 		}
 		curr = curr->next;
@@ -28,11 +33,49 @@ int is_fresh(unsigned long id, Node* head){
 	return 0;
 }
 
+void merge_ranges(Node* head){
+	int merged = 0;
+	Node* curr1 = head;
+	Node* curr2 = NULL;
+
+	do{
+		merged = 0;
+		while(curr1){
+			curr2 = curr1->next;
+			while(curr2){
+				if(in_range(curr2->min, curr1)
+				|| in_range(curr2->max, curr1)
+				|| in_range(curr1->min, curr2)
+				|| in_range(curr1->max, curr2)
+				){
+					if(curr2->min < curr1->min){
+						curr1->min = curr2->min;
+					}
+					if(curr2->max > curr1->max){
+						curr1->max = curr2->max;
+					}
+					curr2->prev->next = curr2->next;
+					if(curr2->next) {
+						curr2->next->prev = curr2->prev;
+					}
+					merged = 1;
+					break;
+				}
+				curr2 = curr2->next;
+			}
+			if(merged){
+				break;
+			}
+			curr1 = curr1->next;
+		}
+	}while(merged);
+}
+
 int main(void) {
 
 	char line[BUFSIZ];
 	int part1 = 0;
-	int part2 = 0;
+	unsigned long part2 = 0;
 	Node* head = NULL;
 	Node* curr = NULL;
 	unsigned long min = 0;
@@ -50,6 +93,7 @@ int main(void) {
 			sscanf(line, "%lu-%lu", &min, &max);
 			if(curr){
 				curr->next = create_node(min, max);
+				curr->next->prev = curr;
 				curr = curr->next;
 			} else {
 				curr = create_node(min, max);
@@ -63,8 +107,16 @@ int main(void) {
 		}
 	}
 
+	merge_ranges(head);
+
+	curr = head;
+	while(curr){
+		part2 += curr->max - curr->min + 1;
+		curr = curr->next;
+	}
+
 	printf("part1: %d\n", part1);
-	printf("part2: %d\n", part2);
+	printf("part2: %lu\n", part2);
 
 	return 0;
 }
