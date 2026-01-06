a75fd4762fcce5fe5a08e4c21a1441e2de210893
b0nk <fsantos.pt@hotmail.com>
2026-01-06 01:16:09 +0000

    2025 - day07 part2

--- 2025/day07.c
+++ 2025/day07.c
@@ -1,57 +1,101 @@
 #include <stdio.h>
 #include <string.h>
 
+
 #define D_SIZE 150
 
+typedef struct Grid Grid;
+struct Grid {
+	char c;
+	unsigned long ways;
+};
 
 int main(void) {
 
 	char line[BUFSIZ];
 	int part1 = 0;
-	int part2 = 0;
-	char diagram[D_SIZE][D_SIZE] = {0};
+	unsigned long part2 = 0;
+	Grid diagram[D_SIZE][D_SIZE] = {0};
 	int lines = 0;
 	int length = 0;
 	char c_start = 'S';
 	char c_beam = '|';
-	char c_splitter = '^';
+	char c_empty = '.';
 
 	while(fgets(line, BUFSIZ, stdin) != NULL) {
 		line[strcspn(line, "\n")] = 0;
 		if(!length) {
 			length = strlen(line);
 		}
-		memcpy(diagram[lines++], line, length);
+		for(int i = 0; i < length; i++) {
+			diagram[lines][i].c = line[i];
+		}
+		lines++;
 	}
 
-	int started = 0;
-	for(int i = 0; i < lines - 1; i++) {
-		for(int j = 0; j < length; j++) {
+	for (int x = 0; x < length; x++) {
+		if (diagram[0][x].c == c_start) {
+			diagram[0][x].c = c_beam;
+			diagram[0][x].ways = 1;
+			break;
+		}
+	}
 
-			if(diagram[i][j] == c_start) {
-				started = 1;
-				diagram[i + 1][j] = c_beam;
-				break;
-			}
+	for (int y = 0; y < lines - 1; y++) {
+		for (int x = 0; x < length; x++) {
 
-			if(!started) {
+			if (diagram[y][x].c != c_beam) {
 				continue;
 			}
 
-			if(diagram[i - 1][j] == c_beam) {
-				if(diagram[i][j] == c_splitter){
-					diagram[i][j - 1] = c_beam;
-					diagram[i][j + 1] = c_beam;
-					part1++;
-				} else {
-					diagram[i][j] = c_beam;
+			switch (diagram[y + 1][x].c) {
+			case '.':
+				diagram[y + 1][x].c = c_beam;
+				diagram[y + 1][x].ways += diagram[y][x].ways;
+				break;
+			case '|':
+				diagram[y + 1][x].ways += diagram[y][x].ways;
+				break;
+			case '^': {
+				int splits = 0;
+
+				if (x > 0) {
+					if (diagram[y + 1][x - 1].c == c_empty) {
+						diagram[y + 1][x - 1].c = c_beam;
+						diagram[y + 1][x - 1].ways += diagram[y][x].ways;
+					} else if (diagram[y + 1][x - 1].c == c_beam) {
+						diagram[y + 1][x - 1].ways += diagram[y][x].ways;
+					}
+					splits++;
 				}
+
+				if (x < length - 1) {
+					if (diagram[y + 1][x + 1].c == c_empty) {
+						diagram[y + 1][x + 1].c = c_beam;
+						diagram[y + 1][x + 1].ways += diagram[y][x].ways;
+					} else if (diagram[y + 1][x + 1].c == c_beam) {
+						diagram[y + 1][x + 1].ways += diagram[y][x].ways;
+					}
+					splits++;
+				}
+
+				if (splits) {
+					part1++;
+				}
+				break;
+			}
+			default:
+				break;
 			}
 		}
 	}
 
+	for(int i = 0; i < length; i++) {
+		part2 += diagram[lines - 1][i].ways;
+	}
+
 	printf("part1: %d\n", part1);
-	printf("part2: %d\n", part2);
+	printf("part2: %lu\n", part2);
 
 	return 0;
 }
